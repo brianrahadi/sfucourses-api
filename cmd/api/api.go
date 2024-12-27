8af54298259cc94000672d6569c3c91bc07f6533
Brian Rahadi <brian.rahadi@gmail.com>
2024-12-27 11:36:30 -0800

    feat: add index and allow cors

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -29,9 +29,26 @@ func (app *application) mount() http.Handler {
 	mux := http.NewServeMux()
 
 	// Middleware for recover and logging
-	mux.Handle("/", app.middleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		http.NotFound(w, r)
-	})))
+	mux.HandleFunc("GET /", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			http.NotFound(w, r)
+			return
+		}
+		w.Header().Set("Content-Type", "text/html")
+		html := `
+		<!DOCTYPE html>
+		<html>
+		<head>
+			<title>Welcome</title>
+		</head>
+		<body>
+			<h1>Welcome to SFU Courses API</h1>
+			<p>Use the API to access course schedules and outlines.</p>
+		</body>
+		</html>
+		`
+		w.Write([]byte(html))
+	}))
 
 	mux.HandleFunc("GET /v1/rest/health", app.healthCheckHandler)
 
@@ -54,6 +71,11 @@ func (app *application) middleware(next http.Handler) http.Handler {
 				http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 			}
 		}()
+		w.Header().Add("Access-Control-Allow-Origin", "*")
+		w.Header().Add("Access-Control-Allow-Credentials", "true")
+		w.Header().Add("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		w.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+
 		log.Printf("%s %s %s", r.Method, r.URL.Path, r.RemoteAddr)
 		next.ServeHTTP(w, r)
 	}), 60*time.Second, "Request timed out")
@@ -62,7 +84,7 @@ func (app *application) middleware(next http.Handler) http.Handler {
 func (app *application) run(mux http.Handler) error {
 	srv := &http.Server{
 		Addr:         app.config.addr,
-		Handler:      mux,
+		Handler:      app.middleware(mux),
 		WriteTimeout: time.Second * 30,
 		ReadTimeout:  time.Second * 10,
 		IdleTimeout:  time.Minute,
